5e8607f1f208be257d6ed45f041105176126225d
Wojciech Bojarski <bojawojt@gmail.com>
2020-02-17 14:02:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector('main').remove()
+let newHeader = document.createElement('h1')
+document.body.appendChild(newHeader)
+newHeader = document.querySelector('h1')
+newHeader.id = "victory"
+newHeader.innerText("is the champion")

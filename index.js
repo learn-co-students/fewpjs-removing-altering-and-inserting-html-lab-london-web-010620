65bdd13d9bb7b062d195a32894c2fa894db5c4ac
simon <sjtucker28@gmail.com>
2020-02-17 15:07:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+//removing main#main
+document.querySelector("main#main").remove()
+
+
+// create a new header and assign it nonsense
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Knobhead is the champion"
